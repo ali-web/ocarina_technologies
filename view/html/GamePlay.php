8dc7279a429a78a130a40b05657d6e824fad6148
shaq147 <shaq147@yahoo.com>
2015-11-19 23:10:22 +0000

    Changed html files to php

---
+++ view/html/GamePlay.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>Your Turn</title>  
+        <link rel="stylesheet" type="text/css" href="gameplay.css">
+        <script src="http://use.edgefonts.net/indie-flower:n4:all.js"></script>
+    </head>
+    
+    <body>
+        <div id="header">
+            <h1>StoryTime with Friends</h1>
+            <a href="" id="notifications">Notifications</a>
+        </div>  
+        
+        <ul id="navbar">
+            <li class="page"><a href="index.html" class="plinks">Home</a></li>
+            <li class="page"><a href="Friends.html" class="plinks">Friends</a></li>
+            <li class="page"><a href="Settings.html" class="plinks">Settings</a></li>
+            <li class="page"><a href="Help.html" class="plinks">Help</a></li>
+        </ul>
+        
+        <h2>Randomly Generated Title</h2>
+        
+        <p>This is where the story will be written... </p>
+        
+        <form>
+            Enter <b>4</b> words:<br>
+            <input type="text" name="words">
+            <input type="submit" value="Add words"> 
+        </form>
+    </body>
+</html>
---
+++ view/html/WaitTurn.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>Wait Turn</title>
+        <link rel="stylesheet" type="text/css" href="waitturn.css">
+        <script src="http://use.edgefonts.net/indie-flower:n4:all.js"></script>
+    </head>
+    
+    <body>
+        <div id="header">
+            <h1>StoryTime with Friends</h1>
+            <a href="" id="notifications">Notifications</a>
+        </div>
+        
+        <ul id="navbar">
+            <li class="page"><a href="index.html" class="plinks">Home</a></li>
+            <li class="page"><a href="Friends.html" class="plinks">Friends</a></li>
+            <li class="page"><a href="Settings.html" class="plinks">Settings</a></li>
+            <li class="page"><a href="Help.html" class="plinks">Help</a></li>
+        </ul>
+        
+        <h2>Randomly Generated Title</h2>
+        
+        <p>This is where the story will be shown... </p>
+        
+        <h3>Waiting your turn...</h3>
+    </body>
+</html>
