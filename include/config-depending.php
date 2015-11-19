c4749d806275f4f7c9d71b4d220ff25d4b5b2a6a
Group <stwfuvic@gmail.com>
2015-11-19 21:11:17 +0000

    Setup project for c9 workspace.

--- include/config-depending.php
+++ include/config-depending.php
@@ -1,9 +1,19 @@
 <?php
 
-define('ST_MYSQL_HOSTNAME', 'localhost');
-define('ST_MYSQL_USERNAME', 'root');
-define('ST_MYSQL_PASSWORD', 'rootfdsf');
-define('ST_MYSQL_DATABASE', 'ST');
+// Check to see if we are running in the cloud 9 development environment.
+// If we are, the C9_USER environment variable will be set.
+$cloud9User = getenv("C9_USER");
+if ($cloud9User) {
+    define('ST_MYSQL_HOSTNAME', getenv('IP'));
+    define('ST_MYSQL_USERNAME', $cloud9User);
+    define('ST_MYSQL_PASSWORD', '');
+    define('ST_MYSQL_DATABASE', 'c9');
+} else {
+    define('ST_MYSQL_HOSTNAME', 'localhost');
+    define('ST_MYSQL_USERNAME', 'root');
+    define('ST_MYSQL_PASSWORD', 'rootfdsf');
+    define('ST_MYSQL_DATABASE', 'ST');
+}
 
 
 define('ST_APP_STATUS', 'developing'); // possible values: developing , deployed
